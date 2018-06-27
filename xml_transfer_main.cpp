baa8130fd383b60a516a05f22026719e46f98c41
伊藤程 <2856483627@qq.com>
2018-06-27 21:00:40 +0800

    Update xml_transfer_main.cpp
    Updated

--- xml_transfer_main.cpp
+++ xml_transfer_main.cpp
@@ -8,59 +8,59 @@
 using namespace std;
 using namespace boost;
 
-int main() {
-    string filename = "text.xml"; //enter your input file
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        cerr << "Wrong number of inputs" << endl;
+    }
+    string inpref = argv[1];
+    string outpref = argv[2];
+
+    string filename = inpref + ".xml"; // *.xml file as default
     fstream fin;
     fin.open(filename.c_str(), ios::in);
 
+    fstream out;
+    out.open(outpref + ".txt", ios::out);
+
     vector<string> v;
     string tmp;
+    bool inPara = false;
 
     while (getline(fin, tmp)) {
-        v.push_back(tmp);
-    }
-
-    vector<string> strs;
-
-    for (vector<string>::const_iterator i = v.begin(); i != v.end(); ++i) {
-        string s = *i;
-        if (s.find("word") != std::string::npos) {
-            strs.push_back(s);
-        }
-    }
-
-    fstream out;
-    out.open("text.txt", ios::out); //enter your output file
-
-    for (vector<string>::const_iterator i = strs.begin(); i != strs.end(); ++i) {
-        string s = *i;
-        char_separator<char> sep(" ");
-        tokenizer<char_separator<char>> tok(s, sep);
-        for (tokenizer<char_separator<char>>::iterator j = tok.begin(); j != tok.end(); ++j) {
-            string s = *j;
-            if (s.find("cont") != std::string::npos) {
-                for (int i = 0; i < 6; i++) {
-                    s.erase(s.begin());
+        if (tmp.find("sent id=") != std::string::npos) {
+            inPara = true;
+        } else if (tmp.find("</sent>") != std::string::npos) {
+            inPara = false;
+            out << "\r\n";
+        } else if (inPara) {
+            char_separator<char> sep(" ");
+            tokenizer<char_separator<char>> tok(tmp, sep);
+            for (tokenizer<char_separator<char>>::iterator beg = tok.begin(); beg != tok.end(); beg ++) {
+                string s = *beg;
+                if (s.find("cont") != std::string::npos) {
+                    for (int i = 0; i < 6; i ++) {
+                        s.erase(s.begin());
+                    }
+                    s.erase(s.end() - 1);
+                    s.push_back('/');
+                    out << s.c_str();
                 }
-                s.erase(s.end() - 1);
-                s.push_back('/');
-                out << s.c_str();
-            }
-            if (s.find("pos") != std::string::npos) {
-                for (int i = 0; i < 5; i++) {
-                    s.erase(s.begin());
+                if (s.find("pos") != std::string::npos) {
+                    for (int i = 0; i < 5; i ++) {
+                        s.erase(s.begin());
+                    }
+                    s.erase(s.end() - 1);
+                    s.push_back('#');
+                    out << s.c_str();
                 }
-                s.erase(s.end() - 1);
-                s.push_back('#');
-                out << s.c_str();
-            }
-            if (s.find("ne") != std::string::npos) {
-                for (int i = 0; i < 4; i++) {
-                    s.erase(s.begin());
+                if (s.find("ne") != std::string::npos) {
+                    for (int i = 0; i < 4; i ++) {
+                        s.erase(s.begin());
+                    }
+                    s.erase(s.end() - 1);
+                    s.push_back(' ');
+                    out << s.c_str();
                 }
-                s.erase(s.end() - 1);
-                s.push_back(' ');
-                out << s.c_str();
             }
         }
     }
